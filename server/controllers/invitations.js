da9af1dff17780ab13116e24965613ad6952521f
Prasad Nikam <nikamprasad52@gmail.com>
2024-06-23 11:59:13 +0530

    tested and fixed invitation.js controller

--- server/controllers/invitations.js
+++ server/controllers/invitations.js
@@ -3,24 +3,64 @@ import Invitation from "../modules/invitations.js";
 import jwt from "jsonwebtoken";
 
 export const sendRequest = async (req, res) => {
-  const { sendTo } = req.body;
-  const decoded = jwt.verify(req.cookies.token, "cvpap");
+    const { sendTo } = req.body;
+    const decoded = jwt.verify(req.cookies.token, "cvpap");
 
-  const invitation = new Invitation({
-    from: decoded.indexOf,
-    to: sendTo,
-    status: "pending",
-  });
-
-  await invitation.save();
+    if (
+        await Invitation.findOne({
+            $or: [
+                { from: decoded.id, to: sendTo },
+                { from: sendTo, to: decoded.id },
+            ],
+        })
+    ) {
+        return res.status(401).send("Invitation is already sent or recieved");
+    } else {
+        const invitation = new Invitation({
+            from: decoded.id,
+            to: sendTo,
+            status: "pending",
+        });
+        try {
+            await invitation.save();
+            return res.status(200).send(invitation);
+        } catch (error) {
+            return res.status(401).send(error);
+        }
+    }
 };
 
-export const acceptRequest = (req, res) => {
-  const { inviter } = req.body;
-  const decoded = jwt.verify(req.cookies.token, "cvpap");
+// Accept the invitation and create relationship between two users
+export const acceptRequest = async (req, res) => {
+    const { invitation_id } = req.body;
+    const decoded = jwt.verify(req.cookies.token, "cvpap");
 
-  const rel = new Relationship({
-    user_id: decoded.id,
-    related_user_id: inviter,
-  });
+    try {
+        const invitation = await Invitation.findById(invitation_id);
+        if (!invitation) {
+            return res.status(401).send("Invalid Invitation id");
+        }
+
+        if (
+            await Relationship.findOne({
+                $or: [
+                    { user_id: decoded.id, related_user_id: invitation.from },
+                    { user_id: invitation.from, related_user_id: decoded.id },
+                ],
+            })
+        ) {
+            return res.status(401).send("Relationship already exist");
+        }
+
+        const rel = new Relationship({
+            user_id: decoded.id,
+            related_user_id: inviter,
+        });
+        await rel.save();
+
+        await Invitation.findByIdAndDelete(invitation_id);
+        return res.send("success");
+    } catch (error) {
+        return res.status(401).send(error);
+    }
 };
